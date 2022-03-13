815118ffb519f879b4d3fdbfd3739531c69bdbaf
neil-unomaha <nthorne@unomaha.edu>
2022-03-13 00:58:35 -0800

    minor changes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   has_many :cards, dependent: :destroy
   belongs_to :domain
+
+  def test_creation_selectbox_to_s
+    "#{domain.name} - name"
+  end
 end
--- app/models/fillin_category.rb
+++ app/models/fillin_category.rb
@@ -1,4 +1,8 @@
 class FillinCategory < ApplicationRecord
   belongs_to :fillin_domain
   has_many :fillin_cards, dependent: :destroy
+
+  def test_creation_selectbox_to_s
+    "#{fillin_domain.name} - name"
+  end
 end
