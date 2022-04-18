87f63c2934c13aa63a1ad78eacdc5e9ad49fcd87
neil-unomaha <nthorne@unomaha.edu>
2022-04-17 21:28:57 -0700

    sanitize user input

--- app/models/fillin_card_test_user_answer.rb
+++ app/models/fillin_card_test_user_answer.rb
@@ -11,7 +11,7 @@ class FillinCardTestUserAnswer < ApplicationRecord
   def correct?
     # default to true when question is not hidden. That means it isn't a question on this card
     return true if !hidden?
-    return true if user_answer == fillin_card_answer.answer
+    return true if ActionController::Base.helpers.sanitize(user_answer) == fillin_card_answer.answer
     return false
   end
 end
